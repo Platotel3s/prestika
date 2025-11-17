ef6c413a4bcb2592c8cba92e74ab35fc70cccaa7
Platotel3s <yudhaplato@gmail.com>
2025-11-17 17:32:18 +0700

    bikin file ImageWelcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ImageWelcome from "./components/ImageWelcome";
 import General from "./layouts/General";
 
 export default function App() {
@@ -7,11 +8,7 @@ export default function App() {
         <h1 className="text-3xl font-bold mb-4">Welcome To Prestika</h1>
         <p className="text-lg opacity-90">Yuk Tambah Presatasimu</p>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-8">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-            <div key={i} className="bg-white/10 p-4 rounded-lg">
-              <img src="https://www.uhamka.ac.id/static/media/FKIP.73a225f609e63ab24aca.jpg" className="w-full h-36 bg-white/20 rounded" />
-            </div>
-          ))}
+          <ImageWelcome />
         </div>
       </section>
       <section className="max-w-5xl mx-auto mt-12 grid grid-cols-3 text-center">
---
+++ src/components/ImageWelcome.jsx
@@ -0,0 +1,9 @@
+export default function ImageWelcome() {
+  return (
+    <div className="flex flex-row justify-center">
+      <img src="https://www.uhamka.ac.id/static/media/FKIP.73a225f609e63ab24aca.jpg" alt="" />
+            <img src="https://www.uhamka.ac.id/static/media/FKIP.73a225f609e63ab24aca.jpg" alt="" />
+
+    </div>
+  );
+}
