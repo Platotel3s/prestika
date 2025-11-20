05f52f63e86b7ae09ef7b422f8258beb8b2846a3
Platotel3s <yudhaplato@gmail.com>
2025-11-21 01:53:32 +0700

    Image Welcome di perbarui

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,20 @@ export default function App() {
     <General>
       <section className="max-w-5xl mx-auto mt-10 text-center">
         <h1 className="text-3xl font-bold mb-4">Welcome To Prestika</h1>
-        <p className="text-lg opacity-90">Yuk Tambah Presatasimu</p>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-8">
+        <div className="flex justify-center flex-row">
           <ImageWelcome />
         </div>
       </section>
-      <section className="max-w-5xl mx-auto mt-12 grid grid-cols-3 text-center">
-        <div className="bg-white/10 p-6 rounded-lg text-center">
+      <section className="max-w-5xl mx-auto mt-12 grid grid-cols-3 text-center gap-2">
+        <div className="bg-white/10 p-6 rounded-lg text-center border-amber-500 border">
           <h3 className="text-xl">Total Penghargaan</h3>
           <p className="text-3xl font-bold mt-2">21.000</p>
         </div>
-        <div className="bg-white/10 p-6 rounded-lg text-center">
+        <div className="bg-white/10 p-6 rounded-lg text-center border border-amber-500">
           <h3 className="text-xl">Total Kompetisi</h3>
           <p className="text-3xl font-bold mt-2">5.000</p>
         </div>
-        <div className="bg-white/10 p-6 rounded-lg text-center">
+        <div className="bg-white/10 p-6 rounded-lg text-center border border-amber-500">
           <h3 className="text-xl hover:underline">Total Mahasiswa Berprestasi</h3>
           <p className="text-3xl font-bold mt-2">12.000</p>
         </div>
--- src/components/ImageWelcome.jsx
+++ src/components/ImageWelcome.jsx
@@ -1,13 +1,21 @@
 export default function ImageWelcome() {
   return (
-    <div className="flex flex-row justify-center">
-      <img src="https://www.uhamka.ac.id/static/media/FKIP.73a225f609e63ab24aca.jpg" alt="" />
-      <img src="https://uhamka.ac.id/static/media/FEB.94011c44541da0fc4ef5.webp" alt="" />
-      <img src="https://uhamka.ac.id/static/media/FT.e3ae1a5100a597d3085a.jpg" alt="" />
-      <img src="https://uhamka.ac.id/static/media/FAI_FIKES_FISIP_FPsi.3ffa482f8ef7a313ecf2.jpg" alt="" />
-      <img src="https://uhamka.ac.id/static/media/FK_FFS.c655025c14df1acea0eb.jpg" alt="" />
-      <img src="https://uhamka.ac.id/static/media/FK_NEW.c58ebcaeeeada9a1c07e.jpeg" alt="" />
-      <img src="https://uhamka.ac.id/static/media/SPS.3d2a79a4b01b3fe1da93.jpg" alt="" />
+    <div className="bg-black/10 p-5 rounded-2xl">
+      <div className="grid grid-cols-4 gap-2 bg-white p-1">
+        <img src="https://www.uhamka.ac.id/static/media/FKIP.73a225f609e63ab24aca.jpg" alt="FKIP" className="rounded-2xl hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FEB.94011c44541da0fc4ef5.webp" alt="FEB" className="rounded-2xl  hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FT.e3ae1a5100a597d3085a.jpg" alt="FTII" className="rounded-2xl  hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FAI_FIKES_FISIP_FPsi.3ffa482f8ef7a313ecf2.jpg" alt="FAI" className="rounded-2xl  hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FK_FFS.c655025c14df1acea0eb.jpg" alt="FFS" className="h-44 rounded-2xl  hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FK_NEW.c58ebcaeeeada9a1c07e.jpeg" alt="FK" className="h-44 w-64 rounded-2xl  hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FAI_FIKES_FISIP_FPsi.3ffa482f8ef7a313ecf2.jpg" alt="FPSI" className="rounded-2xl h-44 hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/SPS.3d2a79a4b01b3fe1da93.jpg" alt="Pascasarjana" className="h-44 w-64 rounded-2xl  hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FAI_FIKES_FISIP_FPsi.3ffa482f8ef7a313ecf2.jpg" alt="FISIP" className="rounded-2xl  hover:-translate-1 transition duration-300" />
+        <img src="https://uhamka.ac.id/static/media/FAI_FIKES_FISIP_FPsi.3ffa482f8ef7a313ecf2.jpg" alt="FIK" className="rounded-2xl  hover:-translate-1 transition duration-300" />
+        <div className="col-span-2 col-start-3 row-start-3 flex flex-col justify-center items-center">
+          <strong className="text-black w-9/12 p-8 rounded-2xl shadow-black shadow-md">Yuk, Tambahkan Prestasimu</strong>
+        </div>
+      </div>
     </div>
   );
 }
